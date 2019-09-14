1d4001d65f2640fbd1126e83677884d45e78ff0d
Павел <elantsev.pa@gmail.com>
2019-09-14 22:26:05 +0300

    +build version

---
+++ build/precache-manifest.5c49c8998a265412edcad4640a10a076.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5397e469d95cb095a04d86c9597a4fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9bb085df8d8962e3dab",
+    "url": "/static/css/main.c92d354b.chunk.css"
+  },
+  {
+    "revision": "a100c231238bec548967",
+    "url": "/static/js/2.48940b17.chunk.js"
+  },
+  {
+    "revision": "e9bb085df8d8962e3dab",
+    "url": "/static/js/main.e223e7d9.chunk.js"
+  },
+  {
+    "revision": "58bafd2c381ef08638c0",
+    "url": "/static/js/runtime~main.ed72cfbc.js"
+  }
+]);
